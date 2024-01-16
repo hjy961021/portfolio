82de55c395cb4eeb57aeb8256a843435c06ca97f
홍지영 <hjy961021@gmail.com>
2024-01-16 20:49:40 +0900

    feat: header UI 추가 및 scss 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <div>
+        <h1>
+          <Link to="/">HONG JIYOUNG</Link>
+        </h1>
+        <ul>
+          <li>
+            <Link
+              to="https://github.com/hjy961021?tab=repositories"
+              target="_blank"
+            >
+              GIT
+            </Link>
+          </li>
+          <li>
+            <Link to="https://hongjiyoung.tistory.com/" target="_blank">
+              BLOG
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
