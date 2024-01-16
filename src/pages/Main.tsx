5bb76e6369c33e64ad675bbcfe888aa5983e5608
홍지영 <hjy961021@gmail.com>
2024-01-16 20:49:04 +0900

    feat: 메인 추가할 내용 정리

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const index = () => {
-  return <div>메인</div>;
+  return (
+    <div>
+      <p>Main</p>
+      <p>자기소개(About)</p>
+      <p>프로젝트(Work)</p>
+      <p>포트폴리오(Portfolio)</p>
+      <p>블로그(Blog)</p>
+      <p>마치며(Contact)</p>
+    </div>
+  );
 };
 
 export default index;
