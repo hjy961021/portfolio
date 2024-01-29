626203ea614a62ae9ce2d8c0216d9c5bb2ceeafa
홍지영 <hjy961021@gmail.com>
2024-01-29 23:44:13 +0900

    feat : main 추가

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const About = () => {
-  return <div className="page about">About</div>;
+  return (
+    <div className="page about">
+      <div>
+        About
+        {/* <p>Main</p>
+        <p>자기소개(About)</p>
+        <p>일(Work)</p>
+        <p>프로젝트(Project)</p>
+        <p>블로그(Blog)</p>
+        <p>마치며(Contact)</p> */}
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
 
 const Main = () => {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight - 300;
+
+  const style = ['style1', 'style2', 'style3'];
+  const opacity = ['opacity1', 'opacity1', 'opacity1', 'opacity2', 'opacity3'];
+  const twinkle = [
+    'twinkle1',
+    'twinkle1',
+    'twinkle1',
+    'twinkle2',
+    'twinkle2',
+    'twinkle3',
+    'twinkle4',
+  ];
+
+  function getRandomValue(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  const stars = Array.from({ length: 150 }).map((_, index) => {
+    const _s = getRandomValue(3);
+    const _o = getRandomValue(5);
+    const _t = getRandomValue(7);
+    const x = getRandomValue(windowWidth);
+    const y = getRandomValue(windowHeight);
+
+    const className =
+      'star ' + style[_s] + ' ' + opacity[_o] + ' ' + twinkle[_t];
+
+    return (
+      <span key={index} className={className} style={{ left: x, top: y }} />
+    );
+  });
+
   return (
     <div className="page main">
-      <div>
-        <p>Main</p>
-        <p>자기소개(About)</p>
-        <p>일(Work)</p>
-        <p>프로젝트(Project)</p>
-        <p>블로그(Blog)</p>
-        <p>마치며(Contact)</p>
+      <div className="mainArea">
+        <span className="moonArea">
+          <span className="moon" />
+        </span>
+        <div className="titleArea">
+          <h2 className="title">HONG JIYOUNG</h2>
+          <span className="subTitle">FRONT-END DEVELOPER PORTFOLIO</span>
+        </div>
+        <div className="starArea">{stars}</div>
       </div>
     </div>
   );
