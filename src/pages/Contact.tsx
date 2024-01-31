7750aef9ba15720295cb2ace44ba5a1d09478470
홍지영 <hjy961021@gmail.com>
2024-01-31 16:10:59 +0900

    feat : contact 추가

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
-  return <div className="page contact">Contact</div>;
+  return (
+    <div className="page contact">
+      <div className="contactArea">
+        <h2 className="title">Contact</h2>
+        <div className="textArea">
+          <span className="text">contact text</span>
+          <ul className="listArea">
+            <li className="list">
+              <Link to="/" className="btn">
+                GIT
+              </Link>
+            </li>
+            <li className="list">
+              <Link to="/" className="btn">
+                BLOG
+              </Link>
+            </li>
+            <li className="list">
+              <span className="btn">EMAIL</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
