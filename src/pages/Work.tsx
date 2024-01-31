1c40743db0bebf6483c5adfa03f1d8335267c910
홍지영 <hjy961021@gmail.com>
2024-01-31 17:09:32 +0900

    feat : work 추가

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,7 +1,122 @@
 import React from 'react';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 
 const Work = () => {
-  return <div className="page work">Work</div>;
+  return (
+    <div className="page work">
+      <div className="workArea">
+        <h2 className="title">Work</h2>
+        <Swiper slidesPerView={1} modules={[Navigation]} navigation speed={600}>
+          <SwiperSlide>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="listArea">
+              <h3 className="listTitle">Skt Tworld 웹 & 앱 고도화</h3>
+              <div className="listGroup">
+                <div className="imgArea">
+                  <img src="" alt="" />
+                </div>
+                <div className="textArea">
+                  <span className="listText">프로젝트 설명</span>
+                  <ul className="tagArea">
+                    <li className="tag">html5</li>
+                    <li className="tag">css</li>
+                    <li className="tag">javascript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
