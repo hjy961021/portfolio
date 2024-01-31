a1df45557160a72d5b0ccbd912e1c3d05a959d11
홍지영 <hjy961021@gmail.com>
2024-01-31 17:41:49 +0900

    feat : project 추가

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,7 +1,98 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Project = () => {
-  return <div className="page project">Projict</div>;
+  return (
+    <div className="page project">
+      <div className="projectArea">
+        <h2 className="title">Project</h2>
+        <Swiper
+          slidesPerView={1}
+          modules={[Pagination]}
+          pagination={{ clickable: true }}
+          speed={600}
+        >
+          <SwiperSlide>
+            <div className="projectGroup">
+              <div className="imgArea">
+                <img src="" alt="" />
+              </div>
+              <div className="projectBox">
+                <div className="titleArea">
+                  <h3 className="projectTitle">프로젝트 이름</h3>
+                  <span className="title">프로젝트 이름</span>
+                  <Link to="/" className="link">
+                    레포 링크
+                  </Link>
+                </div>
+                <div className="textArea">
+                  <h3 className="projectText">프로젝트 설명</h3>
+                  <span className="text">프로젝트 설명</span>
+                </div>
+                <ul className="tagArea">
+                  <li className="tag">html5</li>
+                  <li className="tag">css</li>
+                  <li className="tag">javascript</li>
+                </ul>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="projectGroup">
+              <div className="imgArea">
+                <img src="" alt="" />
+              </div>
+              <div className="projectBox">
+                <div className="titleArea">
+                  <h3 className="projectTitle">프로젝트 이름</h3>
+                  <span className="title">프로젝트 이름</span>
+                  <Link to="/" className="link">
+                    레포 링크
+                  </Link>
+                </div>
+                <div className="textArea">
+                  <h3 className="projectText">프로젝트 설명</h3>
+                  <span className="text">프로젝트 설명</span>
+                </div>
+                <ul className="tagArea">
+                  <li className="tag">html5</li>
+                  <li className="tag">css</li>
+                  <li className="tag">javascript</li>
+                </ul>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="projectGroup">
+              <div className="imgArea">
+                <img src="" alt="" />
+              </div>
+              <div className="projectBox">
+                <div className="titleArea">
+                  <h3 className="projectTitle">프로젝트 이름</h3>
+                  <span className="title">프로젝트 이름</span>
+                  <Link to="/" className="link">
+                    레포 링크
+                  </Link>
+                </div>
+                <div className="textArea">
+                  <h3 className="projectText">프로젝트 설명</h3>
+                  <span className="text">프로젝트 설명</span>
+                </div>
+                <ul className="tagArea">
+                  <li className="tag">html5</li>
+                  <li className="tag">css</li>
+                  <li className="tag">javascript</li>
+                </ul>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-
 const Work = () => {
   return (
     <div className="page work">
