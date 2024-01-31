465b957115de90f324d67f179abfade80bbe9f9d
홍지영 <hjy961021@gmail.com>
2024-01-31 15:42:24 +0900

    feat : blog 추가

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
-  return <div className="page blog">Blog</div>;
+  return (
+    <div className="page blog">
+      <div className="blogArea">
+        <h2 className="title">Blog</h2>
+        <ul className="listArea">
+          <li className="list">
+            <div className="titleArea">
+              <h3 className="listTitle">
+                <Link to="javascript:;">[React] 링크 제목</Link>
+              </h3>
+              <span className="listTime">2023.01.31</span>
+            </div>
+            <span className="listText">짧은 설명</span>
+          </li>
+          <li className="list">
+            <div className="titleArea">
+              <h3 className="listTitle">
+                <Link to="javascript:;">[React] 링크 제목</Link>
+              </h3>
+              <span className="listTime">2023.01.31</span>
+            </div>
+            <span className="listText">짧은 설명</span>
+          </li>
+          <li className="list">
+            <div className="titleArea">
+              <h3 className="listTitle">
+                <Link to="javascript:;">[React] 링크 제목</Link>
+              </h3>
+              <span className="listTime">2023.01.31</span>
+            </div>
+            <span className="listText">짧은 설명</span>
+          </li>
+          <li className="list">
+            <div className="titleArea">
+              <h3 className="listTitle">
+                <Link to="javascript:;">[React] 링크 제목</Link>
+              </h3>
+              <span className="listTime">2023.01.31</span>
+            </div>
+            <span className="listText">짧은 설명</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
